c222b592628118eb96f43e251ddcab5bd839f553
Rudolf Ignatyev <ignatiev.rudolf2012@yandex.ru>
2020-08-17 20:21:19 +0300

    feat: добавить хук для запроса в API за данными профиля

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import '../index.css';
 import ImagePopup from './ImagePopup';
+import { api } from '../utils/api.js';
 
 function Main(props) {
+  const [userName, setUserName] = React.useState(false);
+  const [userDescription, setUserDescription] = React.useState(false);
+  const [userAvatar, setUserAvatar] = React.useState(false);
+
+  React.useEffect(() => {
+    // Загружаем информацию о пользователе
+    api.getUserInfo()
+      .then((initialUserInfo) => {
+        setUserName(initialUserInfo.name);
+        setUserDescription(initialUserInfo.about);
+        setUserAvatar(initialUserInfo.avatar);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__container">
           <div className="profile__area">
             <button className="profile__avatar-edit-button" type="button" aria-label="Редактировать аватар" onClick={props.onEditAvatar}>
-              <img className="profile__avatar" alt="Аватар пользователя" />
+              <img className="profile__avatar" alt="Аватар пользователя" src={`${userAvatar}`} />
             </button>
             <div className="profile__info">
-              <h1 className="profile__title" />
+  <h1 className="profile__title">{userName}</h1>
               <button className="profile__edit-button" type="button" aria-label="Редактировать" onClick={props.onEditProfile} />
-              <p className="profile__subtitle" />
+              <p className="profile__subtitle">{userDescription}</p>
             </div>
           </div>
           <button className="profile__add-button" type="button" aria-label="Добавить" onClick={props.onAddPlace} />
--- src/utils/api.js
+++ src/utils/api.js
@@ -96,4 +96,13 @@ class Api {
   }
 }
 
-export { Api };
+// Создаём объект api класса Api
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-13',
+  headers: {
+    authorization: 'fec7b8f9-403f-4d91-ad40-57682e4afbf3',
+    'Content-Type': 'application/json'
+  }
+});
+
+export { api };
